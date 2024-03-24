cdb9fcd80ee47a7a690930150b8edec7dcc196d2
NewSoulOwO <22099611d@connect.polyu.hk>
2024-03-24 17:08:28 +0800

    output

--- src/output.c
+++ src/output.c
@@ -1,18 +1,19 @@
 #include "output.h"
 #include <unistd.h>
 
-// #define _DEBUG_ // to debug uncomment this line and run `gcc output.c`
+#define _DEBUG_ // to debug uncomment this line and run `gcc output.c`
 #ifdef _DEBUG_
 Process processes[10000];
 int processesCount;
-DayArrange day[10000];
-int dayCount;
+DayArrange day[3][10000];
+int dayCount[3];
 int endPeiod;
 time_t startPeiod;
 
 int main()
 {
-  printREPORT(stdout);
+  printREPORT(stdout,0);
+  
 }
 #endif
 
@@ -40,12 +41,12 @@ int createChild(int *fd)
   {
     int fd[2];
     int p = createChild(fd);
-    printf("%d\n", p);
+    fprintf("%d\n", p);
     // set day...
     // call algrothm
     // print some debug output
     fd[0] = pipefd[0]; // 将管道的读取端传递给父进程
-    printf("childprocess\n");
+    fprintf("childprocess\n");
     return pid;
   }
   else
@@ -53,7 +54,7 @@ int createChild(int *fd)
     // 父进程
     close(pipefd[0]);  // 关闭父进程中的读取端
     fd[1] = pipefd[1]; // 将管道的写入端传递给子进程
-    printf("parentprocess\n");
+    fprintf("parentprocess\n");
     return pid;
   }
 }
@@ -64,56 +65,45 @@ int createChild(int *fd)
 void printREPORT(FILE *file ,int alg)
 {
   Process rejectedProcesses[10000];
-  char Algorithm;
+  char Algorithm[2][10] = {"FCFS", "PR"};
   int rejectedCount = 0;
   char c[100];
   int startTime = 0;
   int endTime;
-  char plant[7];
+  char plant[3][10] = {"PLANT_X","PLANT_Y","PLANT_Z"};
   for(int i = 0;i < processesCount;i++){
     if(processes[i].accepted == 0){
       rejectedProcesses[i] = processes[i];
     }
   }
-  if(alg = 0){
-    Algorithm = "FCFS";
-  }
-  else{
-    Algorithm = "PR"
-  };
-  printf("Algorithm used: %c\n",Algorithm);
-  printf("There are %d Orders ACCEPTED.", acceptedCount);
-  printf(" Details are as follows: \n");
-  printf("ORDER NUMBER   START        END         DAYS    QUANTITY    PLANT\n");
+  
+  fprintf(file, "Algorithm used: %s\n",Algorithm[alg]);
+  fprintf(file, "There are %d Orders ACCEPTED.", dayCount[0]+dayCount[1]+dayCount[2]);
+  fprintf(file, " Details are as follows: \n");
+  fprintf(file, "ORDER NUMBER   START        END         DAYS    QUANTITY    PLANT\n");
   for(int i = 0 ;i < 3; i++){
-    c = day[i][0].Product.orderNumber;
-    switch (i)
-    {
-    case 0:
-      plant = "PLANT_X";
-      break;
-    case 1:
-      plant = "PLANT_Y";
-      break;
-    case 2:
-      plant = "PLANT_Z";
-      break;
-    default:
-      break;
-    }
-    for(int j = 0;i < dayCount){
-      int check = memccpy(c,day[i][j].Product.orderNumber,sizeof(c));
+    memcpy(c,day[i][0].Product.orderNumber,sizeof(c));
+    for(int j = 0;i < dayCount[i];i++){
+      int check = memcmp(c,day[i][j].Product.orderNumber,sizeof(c));
       if(check == 0){
         
       }
       else{
-        c = day[i][j].Product.orderNumber;
+        memcpy(c, day[i][j].Product.orderNumber,sizeof(c));
         endTime = j-1;
         int days = endTime - startTime;
-        printf("%c %c %c %d %d %c",day[i][j].Product.orderNumber,intToTime(startTime),intToTime(endTime),days,day[i][j].producedQuantity,plant)
+        fprintf(file, "%s %s %s %d %d %s\n",day[i][j].Product.orderNumber,intToTime(startTime),intToTime(endTime),days,day[i][j].producedQuantity,plant[i]);
       }
     }
-  
+  }
+  fprintf(file, "- END -\n");
+  fprintf(file, "There are %d Orders REJECTED.",rejectedCount);
+  fprintf(file, " Details are as follows: ");
+  fprintf(file, "ORDER NUMBER   PRODUCT NAME Due Date QUANTITY\n");
+  for(int i = 0;i < rejectedCount;i++){
+    fprintf(file, "%s %c %s %d\n",rejectedProcesses[i].orderNumber,'A' + rejectedProcesses[i].products,intToTime(rejectedProcesses[i].dueDate),rejectedProcesses[i].quantity);
+  }
+  fprintf(file,"- END -\n");
 
 
   // here for parent to analyse.
@@ -133,7 +123,7 @@ void printREPORT(FILE *file ,int alg)
     }
   }
   fprintf(file, "123123123\n");
-  }
+  
 }
 
 // parent process
