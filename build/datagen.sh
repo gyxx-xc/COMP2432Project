316658fe0764d905b45b4516eed9a0003769e557
gyxx-xc <xdll_xc@outlook.com>
2024-04-11 15:52:32 +0800

    add datagen

---
+++ build/datagen.sh
@@ -0,0 +1,14 @@
+#!emacs --script
+;; this is NOT a bash shell script
+
+(random t)
+(let ((x 0))
+  (while (< x 100)
+    (princ
+     (format "addORDER P%04d %s %d00 Product_%c\n"
+             (+ x 1)
+             (format-time-string
+              "%Y-%m-%d" `(,(+ 1717171200 (random (* 30 86400))) . 1))
+             (+ (random 100) 1)
+             (+ 65 (random 9))))
+    (setq x (+ x 1))))
